9f37897e6320e99144e033e49746504998145617
Alem Karim GUENOUNOU <alemkarim.guenounou.dz@viacesi.fr>
2024-07-09 16:14:01 +0100

    zedt chwiya

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react"
 
 export default function Cars() {
 
-    const car1 = 'BMW'
-    const car2 = 'mercedes'
+  const [car, setCar] = useState({
+      name : "Ford",
+      model : "Fiesta",
+      year : 2014,
+  });
+
+  function changeCarYear(event) {
+      setCar(prevCar => ({...prevCar, year: event.target.value}));
+  }
+  function changeCarName(event) {
+    setCar(prevCar => ({...prevCar, name: event.target.value}));
+}
+function changeCarModel(event) {
+  setCar(prevCar => ({...prevCar, model: event.target.value}));
+}
+
   return (
-    <ul>
-        <li>Audi</li>
-        <li>{car1}</li>
-        <li>{car2.toUpperCase()}</li>
-    </ul>
+    <div>
+      <ul>
+          <li>{car.name}</li>
+          <li>{car.model}</li>
+          <li>{car.year}</li>
+      </ul>
+      <input type="text" value={car.model} onChange={changeCarModel}/>
+      <br />
+      <input type="text" value={car.name} onChange={changeCarName}/>
+      <br />
+      <input type="number" value={car.year} onChange={changeCarYear} />
+    </div>
+    
   )
 }
